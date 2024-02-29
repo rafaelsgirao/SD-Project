43cac9095a28cc892adf4ec999d86500c6236b87
Rafael Girão <git@rafael.ovh>
2024-02-29 16:13:28 +0000

    feat: Fix presentation errors for tests

--- Client/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/ClientMain.java
+++ Client/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/ClientMain.java
@@ -2,49 +2,44 @@ package pt.ulisboa.tecnico.tuplespaces.client;
 
 import pt.ulisboa.tecnico.tuplespaces.centralized.contract.*;
 import pt.ulisboa.tecnico.tuplespaces.client.grpc.ClientService;
-import io.grpc.ManagedChannelBuilder;
-import io.grpc.ManagedChannel;
-import io.grpc.StatusRuntimeException;
-import java.util.Scanner;
 
 public class ClientMain {
 
-    // Debug
-    private static final boolean DEBUG_FLAG = (System.getProperty("debug") != null);
-    private static void debug(String message) {
-        if (DEBUG_FLAG) {
-            System.out.println("Debug: " + message);
-        }
+  // Debug
+  private static final boolean DEBUG_FLAG = (System.getProperty("debug") != null);
+
+  private static void debug(String message) {
+    if (DEBUG_FLAG) {
+      System.out.println("Debug: " + message);
+    }
+  }
+
+  public static void main(String[] args) {
+
+    // receive and print arguments
+    debug("Received" + args.length + "arguments");
+    if (DEBUG_FLAG) {
+      for (int i = 0; i < args.length; i++) {
+        System.out.printf("arg[%d] = %s%n", i, args[i]);
+      }
     }
 
-    public static void main(String[] args) {
-
-
-        System.out.println(ClientMain.class.getSimpleName());
-
-        // receive and print arguments
-        System.out.printf("Received %d arguments%n", args.length);
-        for (int i = 0; i < args.length; i++) {
-            System.out.printf("arg[%d] = %s%n", i, args[i]);
-        }
-
-        // check arguments
-        if (args.length != 2) { // it was 3 args!!!!!
-            System.err.println("Argument(s) missing!");
-            System.err.println("Usage: mvn exec:java -Dexec.args='<host> <port>'");
-            return;
-        }
-
-
-        // get the host and the port
-        final String host = args[0];
-        final int port = Integer.parseInt(args[1]);
-        final String target = host + ":" + port;
-        debug("Target: " + target);
-
-        ClientService clientService = new ClientService(target);
-
-        CommandProcessor parser = new CommandProcessor(clientService);
-        parser.parseInput();
+    // check arguments
+    if (args.length != 2) { // it was 3 args!!!!!
+      System.err.println("Argument(s) missing!");
+      System.err.println("Usage: mvn exec:java -Dexec.args='<host> <port>'");
+      return;
     }
+
+    // get the host and the port
+    final String host = args[0];
+    final int port = Integer.parseInt(args[1]);
+    final String target = host + ":" + port;
+    debug("Target: " + target);
+
+    ClientService clientService = new ClientService(target);
+
+    CommandProcessor parser = new CommandProcessor(clientService);
+    parser.parseInput();
+  }
 }
