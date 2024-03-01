d26cc6bd0b8ad1549934b84d0acdda5e8b95fe59
Rafael Girão <git@rafael.ovh>
2024-03-01 15:58:30 +0000

    chore(NameServer): rename files to server.py + validate args

--- NameServer/server.py
+++ NameServer/server.py
@@ -3,7 +3,7 @@ import sys
 sys.path.insert(1, "../Contract/target/generated-sources/protobuf/python")
 import grpc
 import NameServer_pb2_grpc as pb2_grpc
-from NamingServerServiceImpl import NamingServerServiceImpl
+from server_impl import NamingServerServiceImpl
 from concurrent import futures
 from debug import set_debug
 from debug import debug
@@ -22,9 +22,10 @@ if __name__ == "__main__":
             for i in range(1, len(sys.argv)):
                 debug("Argument: " + sys.argv[i])
 
-        # get port
-        port = int(sys.argv[1])
-
+        if len(sys.argv) != 1:
+            print("ERROR: No arguments are accepted. Correct usage:")
+            print(f"python {sys.argv[0]}")
+            sys.exit(1)
         # create server
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
         # add service
@@ -32,11 +33,11 @@ if __name__ == "__main__":
             NamingServerServiceImpl(), server
         )
         # listen on port
-        server.add_insecure_port("[::]:" + str(port))
+        server.add_insecure_port("[::]:" + str(PORT))
         # start server
         server.start()
         # print message
-        print("Server listening on port " + str(port))
+        print("Server listening on port " + str(PORT))
         # print termination message
         print("Press CTRL+C to terminate\n")
         # wait for server to finish
